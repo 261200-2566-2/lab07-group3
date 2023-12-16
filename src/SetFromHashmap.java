6944c56f1d6fe9d0d186903f633011eb7b858197
NMBxNIGHT <thiranat_kakanmee@cmu.ac.th>
2023-12-16 21:38:56 +0700

    Fix addAll & containAll Add remove,retainAll method (not sure is it work)

--- src/SetFromHashmap.java
+++ src/SetFromHashmap.java
@@ -1,47 +1,64 @@
-import java.util.Collection;
+
 import  java.util.HashMap;
 
 
 public class  SetFromHashmap<K,V> extends HashMap{
-    HashMap <K,V> s;
+    protected HashMap <K,V> s;
     SetFromHashmap(){
         s = new HashMap<>();
     }
-
+    @Override
+    public int size(){
+        return s.size();
+    }
+    @Override
+    public boolean isEmpty() {
+        return s.isEmpty();
+    }
     public void add(K key,V val){
         if(s.get(key) == null){
             s.put(key,val);
         }
     }
-    public void addAll(SetFromHashmap H){
-        for(K val: H.s.keySet()){
+    public void addAll(SetFromHashmap<K,V> H){
+        for(K key: H.s.keySet()){
             if(s.get(key) == null){
-               // V val = H.get(key);
-                s.put(key,key);
+                V val = H.s.get(key);
+                s.put(key,val);
             }
         }
     }
-    public boolean containsAll(SetFromHashmap set){
-        for(K key:set.keySet()){
+    public boolean contains(K key){
+        return s.containsKey(key);
+    }
+    public boolean containsAll(SetFromHashmap<K,V> set){
+        for(K key:set.s.keySet()){
             if(!s.containsKey(key)) return false;
         }
-
         return true;
     }
 
-    @Override
-    public Collection<V> values() {
-        return super.values();
+    public Object remove(SetFromHashmap<K,V> val){
+        return s.remove(val.keySet());
     }
-
-
     public void removeAll(){
         s.clear();
     }
 
-    public boolean contains(K key){
-        return s.containsKey(key);
+    public boolean retainAll(SetFromHashmap<K,V> set){
+        boolean isChange =false;
+        SetFromHashmap<K,V> tempSet = new SetFromHashmap<>();
+        for(K key:set.s.keySet()){
+            if(this.containsKey(key)){
+                tempSet.add(key,this.s.get(key));
+            }
+        }
+        isChange = !(s.size() == tempSet.size());
+        s=tempSet;
+        return isChange;
     }
+
+
 
 
 
@@ -53,12 +70,6 @@ public class  SetFromHashmap<K,V> extends HashMap{
     public boolean equals(){
         return false;
     }
-    @Override
-    public int size(){
-        return s.size();
-    }
-    @Override
-    public boolean isEmpty() {
-        return s.isEmpty();
-    }
+
+
 }
